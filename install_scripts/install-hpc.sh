d998074ad21464f1085dc39153ee5939443eeeba
Ryan Elliot <rmelliot1@sheffield.ac.uk>
2026-01-30 14:38:47 +0000

    attempt a fix on lammps hpc install

--- install_scripts/install-hpc.sh
+++ install_scripts/install-hpc.sh
@@ -9,6 +9,7 @@ module purge
 module load CMake/3.26.3-GCCcore-12.3.0
 module load Python/3.11.3-GCCcore-12.3.0
 module load OpenMPI/4.1.5-GCC-12.3.0
+module load OpenBLAS/0.3.23-GCC-12.3.0
 
 # Load venv
 export VENV=$FASTDATA/lammps-venv

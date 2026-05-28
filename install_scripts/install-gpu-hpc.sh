4293a11d50a34e1f2afdf6b6e16f49d8e8706dbe
Ryan Elliot <86525779+RyanMElliot@users.noreply.github.com>
2026-05-28 17:44:39 +0100

    Create install-gpu-hpc.sh

---
+++ install_scripts/install-gpu-hpc.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --ntasks=8
+#SBATCH --gres=gpu:1         # Number of GPUs
+#SBATCH --mem=8G
+#SBATCH --time=0-00:30:00       # time (DD-HH:MM)
+#SBATCH --job-name=gpu_build
+
+# Make build dir
+cd ..
+mkdir build
+
+# Load required modules
+module purge
+module load CUDA/11.8.0
+module load CMake/3.26.3-GCCcore-12.3.0
+module load Python/3.11.3-GCCcore-12.3.0
+module load OpenMPI/4.1.5-GCC-12.3.0
+module load OpenBLAS/0.3.23-GCC-12.3.0
+
+# Load venv
+export VENV=/mnt/parscratch/users/smp24rme/lammps-gpu
+source $VENV/bin/activate
+
+# Define build
+cmake -S cmake -B build -D CMAKE_BUILD_TYPE=Release \
+    -D PKG_GPU=ON -D GPU_API=cuda \
+    -D CMAKE_INSTALL_PREFIX=$VENV \
+    -D BUILD_MPI=yes -D BUILD_OMP=yes -D BUILD_LIB=yes -D BUILD_SHARED_LIBS=yes \
+    -D Python_EXECUTABLE=$(which python) -D PKG_PYTHON=yes \
+    -D DOWNLOAD_VORO=yes -D PKG_VORONOI=yes \
+    -D PKG_MANYBODY=yes -D PKG_EXTRA-FIX=yes -D EXTRA-COMPUTE=yes -D PKG_REPLICA=yes -D PKG_MEAM=yes
+
+# Build in parallel
+cmake --build build --parallel 8
+
+cmake --install build
+cd build
+make install-python
+
+cd ..
+rm -r build
+
+# Modify venv/bin/activate to export lammps libs locations
+echo "export LD_LIBRARY_PATH=$VENV/lib64:$LD_LIBRARY_PATH" >> $VENV/bin/activate
+echo "export LD_LIBRARY_PATH=/opt/apps/testapps/el7/software/staging/OpenBLAS/0.3.23-GCC-12.3.0/lib:$LD_LIBRARY_PATH" >> $VENV/bin/activate
